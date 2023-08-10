806170fed6c656a5374cad560b34e1c6b0cb8c33
Zo Lambert <zo.lambert@ivory-tower.fr>
2023-08-10 14:52:30 +0200

    added tests project

---
+++ src/WDK_Tests/Window.cpp
@@ -0,0 +1,21 @@
+#include "CppUnitTest.h"
+
+#include <WDK/Rectangle.hpp>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace WDK
+{
+	namespace Tests
+	{
+		TEST_CLASS(Rectangle)
+		{
+		public:
+			TEST_METHOD(Rectangle_Create)
+			{
+				//TODO Zo: tests
+				Assert::AreEqual(0, 0, L"0 != O?????");
+			}
+		};
+	}
+}
